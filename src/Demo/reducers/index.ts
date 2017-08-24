98daa2fe2e4b4fc4f1c4789d1985accaf75d3a30
simo <simo@SM-MACPRO.local>
2017-08-24 16:48:23 +0200

    sm

--- src/Demo/reducers/index.ts
+++ src/Demo/reducers/index.ts
@@ -3,13 +3,13 @@ import * as fromContext from './context';
 
 // Store Model
 export interface Model {
-    test: fromContext.Model;
+    context: fromContext.Model;
 }
 
 // Root Reducer
 export default Redux.combineReducers<Model>({
-    test: fromContext.default
+    context: fromContext.default
 });
 
 // Selectors
-export const getContextName = (s: Model) => fromContext.getContextName(s.test);
+export const getContextName = (s: Model) => fromContext.getContextName(s.context);
