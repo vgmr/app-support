fb107c75c74d4140cca7555e8082ed37b2904f5f
simo <simo@SM-MACPRO.local>
2017-10-19 16:47:25 +0200

    migrated to react 16

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './components/App';
 
-
 import { loadConfiguration } from 'app-support';
 
 loadConfiguration('./config.json').then(cfg => {
