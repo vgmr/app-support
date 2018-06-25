4e8b01dd873b1ef5b5745208d2b4ce3b0ee23d8a
Simone Mulas <simo@SM-IMAC.local>
2018-06-25 15:04:24 +0200

    upgraded redux helper

--- src/crud/crudProxy.ts
+++ src/crud/crudProxy.ts
@@ -3,7 +3,7 @@
 export interface Crud<TElement, TFilter, TKey> {
     search: (filter: TFilter) => Promise<TElement[]>,
     getById: (id: TKey) => Promise<TElement | undefined>
-    save: (item: TElement) => Promise<TElement | string>,
-    delete: (id: TKey) => Promise<boolean>,
-    create: (item: Partial<TElement>) => Promise<TElement>
+    save?: (item: TElement) => Promise<TElement | string>,
+    delete?: (id: TKey) => Promise<boolean>,
+    create?: (item: Partial<TElement>) => Promise<TElement>
 }
